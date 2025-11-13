7c26d0ecb1960d231d0190c87f7a4dbdfd489706
Dan9142 <140359485+Dan9142@users.noreply.github.com>
2025-11-12 20:55:51 -0600

    Fully implemented fieldMap

--- src/kurt/test/Kurt.java
+++ src/kurt/test/Kurt.java
@@ -5,8 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-import static kurt.test.FieldType.*;
-
 public class Kurt {
     static final String PATH = "kurtCollab/src/kurt/test/sample.vff";
     static final String WRITE = "kurtCollab/src/kurt/test/writeTest.vff";
@@ -14,30 +12,45 @@ public class Kurt {
     static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
-        Parser parser = new Parser(readFile(PATH), new HashMap<>());
         Scanner input = new Scanner(System.in);
 
-        Map<String, User> users = parser.map();
-        if (hadError) return;
+        /*
+        * In order to create new users, there are two
+        * approaches we can take. The first one is as
+        * follows:
+        */
 
-        System.out.print("Enter username: ");
+        Creator creator = new Creator(new User()); // User instantiated within Creator
+
+        System.out.print("Enter new username: ");
         String name = input.nextLine();
+        // Add new field into user with this method.
+        creator.add(new Field.Name(name)); // It's worth noting that this method returns an error code
 
-        User user = users.get(name);
-        if (user == null) {
-            System.out.println("User does not exist");
-            return;
-        }
-
-        System.out.print("Enter password: ");
+        System.out.print("Enter new password: ");
         String pass = input.nextLine();
+        creator.add(new Field.Password(pass));
 
-        if (!pass.equals(user.getPassword())) {
-            System.out.println("Incorrect password.");
-            return;
-        }
+        System.out.println(creator.getUser());
 
-        System.out.println("You're in.");
+        /*
+        * The downside of this is that we have to get
+        * the user back using the Creator's getUser().
+        * method. The second approach is as follows:
+        */
+
+        User user = new User(); // Keep reference to user instead
+        creator = new Creator(user);
+
+        System.out.print("Enter new username: ");
+        name = input.nextLine();
+        creator.add(new Field.Name(name)); // Same functionality
+
+        System.out.print("Enter new password: ");
+        pass = input.nextLine();
+        creator.add(new Field.Password(pass));
+
+        System.out.println(user);
     }
 
     public static byte[] readFile(String path) throws IOException {
--- src/kurt/test/User.java
+++ src/kurt/test/User.java
@@ -1,17 +1,10 @@
 package kurt.test;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 class User {
-    List<Field> fields = new ArrayList<>(2);
     private final Map<FieldType, Field> fieldMap = new HashMap<>(2);
-
-    public void add(Field field) {
-        fields.add(field);
-    }
 
     public void put(FieldType id, Field field) {
         fieldMap.put(id, field);
@@ -19,31 +12,43 @@ class User {
 
     public String getUsername() {
         Field.Name name = (Field.Name)fieldMap.get(FieldType.USERNAME);
-        return name.value;
+        return name == null ? "N/A" : name.value;
     }
 
     public String getPassword() {
         Field.Password pass = (Field.Password)fieldMap.get(FieldType.PWD);
-        return pass.value;
+        return pass == null ? "N/A" : pass.value;
     }
 
     public String getDob() {
         Field.DOB date = (Field.DOB)fieldMap.get(FieldType.DOB);
-        return date.value;
+        return date == null ? "N/A" : date.value;
     }
 
     public String getEmail() {
         Field.Email email = (Field.Email)fieldMap.get(FieldType.EMAIL);
-        return email.value;
+        return email == null ? "N/A" : email.value;
     }
 
     public double getReputation() {
         Field.Reputation rep = (Field.Reputation)fieldMap.get(FieldType.REP);
-        return rep.value;
+        return rep == null ? 0 : rep.value;
     }
 
     public int getPosts() {
         Field.Posts posts = (Field.Posts)fieldMap.get(FieldType.POSTS);
-        return posts.value;
+        return posts == null ? 0 : posts.value;
+    }
+
+    @Override
+    public String toString() {
+        String format = """
+                {
+                    Username: [ %s ], Password: [ %s ],
+                    Date of Birth: [ %s ], Email: [ %s ],
+                    Reputation: [ %s ], Posts: [ %s ]
+                }""";
+        return String.format(format, getUsername(), getPassword(), getDob(),
+                    getEmail(), getReputation(), getPosts());
     }
 }
